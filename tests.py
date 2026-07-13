26f10e8c328832a3353fe47e4e2a3920f402c7d1
Jan Zielo <janzielinski117@gmail.com>
2026-07-13 22:40:04 +0200

    add write_file func and test it

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def main():
     # get_files_info tests:
@@ -17,10 +18,20 @@ def main():
 
 
     # get_file_content tests:
-    get_file_content_truncate = get_file_content('calculator', 'lorem.txt')
-    print(get_file_content_truncate)
+    # working_dir = 'calculator'
+    #
+    # print(get_file_content(working_dir, 'main.py'))
+    # print(get_file_content(working_dir, 'pkg/calculator.py'))
+    # print(get_file_content(working_dir, '/bin/cat'))
 
-    get_file_content_result = get_file_content('calculator', 'main.py')
-    print(get_file_content_result)
+    # write_file tests:
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+    print(write_file("calculator", "pkg2/temp.txt", "this should be allowed"))
+
+
+
+
 
 main()
